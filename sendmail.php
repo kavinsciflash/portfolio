2e7c36e474ece03756e8dc999aea9fe6d3b38294
kavinprasanth v <73733462+kavinsciflash@users.noreply.github.com>
2024-05-13 13:51:36 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,18 +1,21 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(
-        !empty($_POST['name'])
+        !empty($_POST['firstname'])
+        && !empty($_POST['lastname'])
         && !empty($_POST['email'])
-        && !empty($_POST['message'])
+&& !empty($_POST['subject'])
+&& !empty($_POST['message'])
     ){
-        $name = $_POST["name"];
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
         $email = $_POST["email"];
         $phone = $_POST["phone"];
         $message = $_POST["message"];
 
 
-        $to = "your@email.com";
+        $to = "barryallean7274@gmail.com";
         $subject = "New Contact Form Submission";
-        $body = "Name: {$name}\nEmail: {$email}\nPhone: {$phone}\nMessage: {$message}";
+        $body = "Name: {$firstname}{$lastname} \nEmail: {$email}\nPhone: {$phone}\nMessage: {$message}";
         $headers = "From: {$email}";
 
 
